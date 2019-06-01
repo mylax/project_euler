509c3d7f8934e76d449e8aba15a19361ce090909
mylax <florian_schwarz@web.de>
2019-06-01 20:41:27 +0200

    wrote some functions in R

---
+++ 1/script.R
@@ -0,0 +1,14 @@
+findMultiplesThree(n)
+
+# @param n find all Multiples of the number n
+# @return list of all multiples
+
+findMultiplesFive(n)
+
+
+
+#tests:
+
+findMultiplesThree(10) # returns [3, 6, 9]
+findMultiplesThree(0) # returns []
+findMultiplesThree(-10) # returns []
