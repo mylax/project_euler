b61f47e3c90c70f18b0b526a777f317108c00ff2
Florian Schwarz <Florian.Schwarz@de.ey.com>
2019-06-12 18:52:03 +0200

    started solving question three in python

---
+++ 3/python/funcs.py
@@ -0,0 +1,19 @@
+def primes(limit):
+    primes = [True] * (limit + 1)
+    i = 2
+    while i <= limit:
+        if primes[i]:
+            yield(i)
+            for k in range(i, limit + 1, i):
+                primes[k] = False
+        i += 1
+
+def find_factors(n):
+    for prime in primes(int(n ** 0.5) + 1):
+        while (n % prime == 0):
+            n /= prime
+        if n == 1:
+            return prime
+    return n
+
+find_factors(600851475143)
